3967f5c893d996d67e6c4530b1fcebb5ec51f115
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-09-24 23:54:28 +0500

    Task3

---
+++ Tyuiu.IvanovPG.Sprint1.Task3.V18/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.IvanovPG.Sprint1.Task3.V18.Lib;
+
+// ЗАДАНИЕ
+// Написать программу, которая запрашивает у пользователя исходные данные,
+// выполняет указанные расчеты и печатает результат на экране
+// Расчеты: объявите исходные переменные и напишите программу вычисления площади треугольника
+
+namespace Tyuiu.IvanovPG.Sprint1.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("**************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ: *");
+            Console.WriteLine("**************************************************************************************");
+
+            double a = 12;
+            double b = 17;
+            double c = 4;
+            Console.WriteLine("Сторона А прямоугольника = " + a);
+            Console.WriteLine("Сторона В прямоугольника = " + b);
+            Console.WriteLine("Сторона C квадрата = " + c);
+
+            Console.WriteLine("**************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ: *");
+            Console.WriteLine("**************************************************************************************");
+
+            Console.WriteLine(" = " + ds.HowManySquares(a, b, c));
+
+            Console.ReadKey();
+
+        }
+    }
+}
