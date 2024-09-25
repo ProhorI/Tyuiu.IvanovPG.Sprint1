7bdb0c63ebff607136c912d0fd687c1cbae85494
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-09-25 23:20:45 +0500

    Task2

--- Tyuiu.IvanovPG.Sprint1.Task2.V9/Program.cs
+++ Tyuiu.IvanovPG.Sprint1.Task2.V9/Program.cs
@@ -38,12 +38,15 @@ namespace Tyuiu.IvanovPG.Sprint1.Task2.V0
             Console.WriteLine("Введите значение r:");
             r = Convert.ToInt32(Console.ReadLine());
 
+            Math.Round(25.132741228718345, 3);
 
             Console.WriteLine("**************************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                                         *");
             Console.WriteLine("**************************************************************************************");
 
             Console.WriteLine("Обьем шара = " + ds.CalculateVolumeCircle(r));
+
+            Console.WriteLine(string.Format("{0:F3}", 25.132741228718345));
 
             Console.ReadLine();
         }
