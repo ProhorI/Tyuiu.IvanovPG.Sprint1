62ecb6e6da45140dbeafa1369651f9cb08bb5bb6
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-09-26 21:19:08 +0500

    Task2

--- Tyuiu.IvanovPG.Sprint1.Task2.V9.Lib/DataService.cs
+++ Tyuiu.IvanovPG.Sprint1.Task2.V9.Lib/DataService.cs
@@ -6,22 +6,7 @@ namespace Tyuiu.IvanovPG.Sprint1.Task2.V9.Lib
     {
         public double CalculateVolumeCircle(int r)
         {
-            return 4 / 3 * 3.14 * Math.Pow(r, 3);
+            return Math.Round(((Math.PI * Math.Pow(r, 3) * 4 / 3)), 3);
         }
     }
 }
-
-//using tyuiu.cources.programming.interfaces.Sprint1;
-
-//namespace Tyuiu.IvanovPG.Sprint1.Task2.V0.Lib
-//{
-//    public class DataService : ISprint1Task2V0
-//    {
-//        public int Sqr(int value)
-//        {
-//            return value * value;
-//        }
-//    }
-//}
-
-//return Math.Round(4 /3 * Math.PI * Math.Pow(r, 3), 3);
