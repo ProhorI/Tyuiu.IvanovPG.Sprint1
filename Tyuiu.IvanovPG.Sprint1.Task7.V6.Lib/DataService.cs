19f018938b5a988b108035055e2f960dca4073cb
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-09-29 21:40:40 +0500

    Task7

--- Tyuiu.IvanovPG.Sprint1.Task7.V6.Lib/DataService.cs
+++ Tyuiu.IvanovPG.Sprint1.Task7.V6.Lib/DataService.cs
@@ -7,8 +7,9 @@ namespace Tyuiu.IvanovPG.Sprint1.Task7.V6.Lib
         public double Calculate(double x, double y)
         {
             double res = Math.Pow((1 + 1/Math.Pow(x, 2)), x) - 12 * Math.Pow(x, 2) * y;
-            return res;
             res = Math.Round(res, 3);
+            return res;
+
 
         }
     }
--- Tyuiu.IvanovPG.Sprint1.Task7.V6.Test/DataServiceTest.cs
+++ Tyuiu.IvanovPG.Sprint1.Task7.V6.Test/DataServiceTest.cs
@@ -11,9 +11,9 @@ namespace Tyuiu.IvanovPG.Sprint1.Task7.V6.Test
         public void ValidExpression()
         {
             DataService ds = new DataService();
-            double x = 3;
-            double y = 2;
-            double wait = -214.628;
+            double x = 2;
+            double y = 4;
+            double wait = -190.438;
             var res = ds.Calculate(x, y);
             Assert.AreEqual(wait, res);
         }
